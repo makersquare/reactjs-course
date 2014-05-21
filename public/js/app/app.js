8cf5dd927d461d0352fd1af995db8c343a71ace9
Way Spurr-Chen <wayspurrchen@gmail.com>
2014-05-21 17:02:18 -0500

    Significant work checkpoint -- close to getting full list view rendering

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,14 +3,21 @@ var AwsumApp = {
   viewClasses: {},
   views : {},
   start : function() {
-    this.initializeViews();
+    this.selectUser();
   },
-  initializeViews : function() {
-    var UserSelectBox = this.viewClasses.UserSelectBox;
+  selectUser : function() {
+    var SelectUserBox = this.viewClasses.SelectUserBox;
     React.renderComponent(
       // Set props
-      <UserSelectBox url="/users" />,
+      <SelectUserBox url="/users" />,
       document.getElementById('content')
     );
+  },
+  loadUserPage : function(user_id) {
+    var UserPage = this.viewClasses.UserPage;
+    React.renderComponent(
+      <UserPage url={"/user/" + user_id} />,
+      document.getElementById('content')
+    )
   }
 };
--- public/js/app/views.js
+++ public/js/app/views.js
@@ -1,9 +1,112 @@
 /** @jsx React.DOM */
 (function(AwsumApp) {
-  AwsumApp.viewClasses.UserListItem  = React.createClass({
+  var UserPage,
+      UserPageControlPanel,
+      UserPageListBox,
+      UserPageList,
+      UserPageListItem,
+      SelectUserBox,
+      SelectUserListItem;
+
+  // Utility function; makes an AJAX request and returns the data
+  // to the component, setting a key on its properties to the data object
+  function setPropsWithAJAXData(desiredKey, url) {
+    if (!desiredKey) desiredKey = "data";
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        var props = {};
+        props[desiredKey] = data;
+        this.setProps(props);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  // Page for /user/:id
+  AwsumApp.viewClasses.UserPage = React.createClass({
+    // Get our user's data with AJAX
+    componentWillMount: function() {
+      setPropsWithAJAXData.call(this, 'data', this.props.url);
+    },
+    render: function() {
+      var listsData = null;
+      if (this.props && this.props.data) {
+        listsData = this.props.data.lists;
+      }
+
+      // need loop inside this one
+
+      return (
+        <h1 className="user-title">{this.props.username}</h1>,
+        <UserPageListBox lists={listsData} />
+      )
+    }
+  });
+
+  // Box that holds individual lists for users
+  AwsumApp.viewClasses.UserPageListBox = React.createClass({
+    render : function() {
+      var listNodes;
+      if (this.props && this.props.lists) {
+        listNodes = this.props.lists.map(function (list) {
+          return <UserPageList url={"/list/" + list.id} name={list.name} id={list.id} />;
+        });
+      }
+
+      return (
+        <div className="user-list-box">
+          {listNodes}
+        </div>
+      )
+    }
+  });
+
+  // Individual lists that hold links, inside of UserPageListBox
+  AwsumApp.viewClasses.UserPageList = React.createClass({
+    // Get this list's links
+    componentWillMount: function() {
+      setPropsWithAJAXData.call(this, 'data', this.props.url);
+    },
+    render : function() {
+      var linkNodes = null;
+      if (this.props && this.props.data) {
+        linkNodes = this.props.data.links.map(function (link) {
+          return <UserPageLink id={data.id} url={data.url} name={data.name} description={data.description} />;
+        });
+      }
+
+      return (
+        <div className="user-list">
+          <h2>List Name: {this.props.name}</h2>
+          {linkNodes}
+        </div>
+      )
+    }
+  });
+
+  AwsumApp.viewClasses.UserPageLink = React.createClass({
+    render : function() {
+      return (
+        <div className="user-link">
+          <a href={this.props.url}>
+            {this.props.name}
+          </a>
+        </div>
+      )
+    }
+  })
+
+  AwsumApp.viewClasses.SelectUserListItem = React.createClass({
+    handleClick : function() {
+      AwsumApp.loadUserPage(this.props.id);
+    },
     render: function() {
       return (
-        <li>{this.props.user}</li>
+        <li onClick={this.handleClick}>{this.props.username}</li>
       )
     }
   });
@@ -11,25 +114,15 @@
   // Select box where we will choose a user's page to view.
   // We first make a call to the server to get all users
   // which is passed to this.props.url when the component is defined
-  AwsumApp.viewClasses.UserSelectBox = React.createClass({
+  AwsumApp.viewClasses.SelectUserBox = React.createClass({
     componentWillMount: function() {
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        success: function(data) {
-          this.setState({data: data});
-        }.bind(this),
-        error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
+      setPropsWithAJAXData.call(this, 'data', this.props.url);
     },
     render: function() {
       var userNodes;
-      if (this.state && this.state.data) {
-        userNodes = this.state.data.map(function (user) {
-          var UserListItem = AwsumApp.viewClasses.UserListItem;
-          return <UserListItem user={user.username} />;
+      if (this.props && this.props.data) {
+        userNodes = this.props.data.map(function (user) {
+          return <SelectUserListItem id={user.id} username={user.username} />;
         });
       } else {
         userNodes = null;
@@ -45,4 +138,15 @@
       )
     }
   });
+
+  // Make React Components available to global render() methods.
+  // These will be available to render() because they are assigned
+  // before any component calls render().
+  SelectUserBox        = AwsumApp.viewClasses.SelectUserBox;
+  SelectUserListItem   = AwsumApp.viewClasses.SelectUserListItem;
+  UserPage             = AwsumApp.viewClasses.UserPage;
+  UserPageControlPanel = AwsumApp.viewClasses.UserPageControlPanel;
+  UserPageListBox      = AwsumApp.viewClasses.UserPageListBox;
+  UserPageList         = AwsumApp.viewClasses.UserPageList;
+  UserPageListItem     = AwsumApp.viewClasses.UserPageListItem;
 })(AwsumApp);
